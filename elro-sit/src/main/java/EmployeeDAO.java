48dfc718cbfe13d403a57a78b1859420907951f4
unknown <wu26@VUSMIN-2af5c89.ad.moneygram.com>
2014-12-01 06:05:00 -0600

    Renaming the project

---
+++ elro-sit/src/main/java/EmployeeDAO.java
@@ -0,0 +1,19 @@
+
+import hello.Employee;
+
+
+public class EmployeeDAO implements EmployeeImpl {
+
+	@Override
+	public void insert(Employee employee) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Employee findByCustomerId(String empId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
