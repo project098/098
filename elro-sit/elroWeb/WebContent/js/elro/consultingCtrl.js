19cddee4df51f791a07d19e2b485ac1cf70b5193
shameer <shameersyed.syed@gmail.com>
2015-05-13 06:23:46 -0500

    Updated the hompage and added practises and People page

---
+++ elro-sit/elroWeb/WebContent/js/elro/consultingCtrl.js
@@ -0,0 +1,10 @@
+/**
+ * Created by wu26 on 5/11/2015.
+ */
+
+elromodule.controller('consultingCtrl',function($scope){
+    //alert ("consultingCtrl");
+    /*
+     Here you can handle controller for specific route as well.
+     */
+});
---
+++ elro-sit/elroWeb/WebContent/js/elro/mainCtrl.js
@@ -0,0 +1,9 @@
+/**
+ * Created by wu26 on 5/11/2015.
+ */
+elromodule.controller('mainCtrl',function($scope){
+    //alert ("mainCtrl");
+    /*
+     Here you can handle controller for specific route as well.
+     */
+});
