08241d142ae2a9b2631e91d025a796dc5232fd29
shameer <shameersyed.syed@gmail.com>
2015-05-29 08:40:45 -0500

    aws launch

---
+++ elro-sit/elrosit/target/elrosit/elroRoot.js
@@ -0,0 +1,37 @@
+/**
+ * Created by wu26 on 5/8/2015.
+ */
+
+var elromodule = angular.module('elroapp',['ngRoute']);
+elromodule.config(function($routeProvider){
+    $routeProvider
+        .when('/',{
+            templateUrl: 'html/main.html'
+        })
+        .when('/practices',{
+            controller:'practicesCtrl',
+            templateUrl: 'html/practises.html'
+        }).when('/people', {
+            controller: 'peopleCtrl',
+            templateUrl: 'html/people.html'
+        }).when('/contactus', {
+            controller: 'contactCtrl',
+            templateUrl: 'html/contactus.html'
+        }).when('/consulting', {
+            controller: 'consultingCtrl',
+            templateUrl: 'html/consulting.html'
+        }).when('/buyelrO', {
+            controller: 'buyelrOCtrl',
+            templateUrl: 'html/buyelrO.html'
+        }).when('/login', {
+            controller: 'loginCtrl',
+            templateUrl: 'html/login.html'
+        }).when('/registerNow', {
+            controller: 'registerNowCtrl',
+            templateUrl: 'html/registerNow.html'
+        }).when('/main', {
+            controller: 'registerNowCtrl',
+            templateUrl: 'html/main.html'
+        })
+});
+
