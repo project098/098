3b6270447e015adf57e460c4eeb7281cd37e69e0
shameersyed84 <shameersyed.syed@gmail.com>
2015-07-15 13:24:15 +0530

    modified core files

--- elro-sit/Slalom.Angular.LargeProjectTemplate_exercise2/scripts/core/coreModule.js
+++ elro-sit/Slalom.Angular.LargeProjectTemplate_exercise2/scripts/core/coreModule.js
@@ -9,9 +9,9 @@ define(["Angular", "AngularUiRouter", "core/coreRoutes"], function (angular, ngR
     //       This will ensure the modules are loaded at bootstrap.  If the modules are configured
     //       for require.js lazy loading then the module contents will not be loaded until the user
     //       requests a "state" requiring the content.
-    var coreModule = angular.module("core", ["ngResource", "ui.router", "scUtilities", "application", "admin"]);
+    var coreModule = angular.module("core", ["ngResource", "ui.router","scUtilities", "application", "admin"]);
 
-    // Configure the core module for CORS and to use the lazy loader 
+    // Configure the core module for CORS and to use the lazy loader
     coreModule.config(function ($injector, moduleConfigurationProvider, requireLoaderProvider) {
         moduleConfigurationProvider.configureCors($injector);
         requireLoaderProvider.configure(coreModule, $injector, routeConfig);
